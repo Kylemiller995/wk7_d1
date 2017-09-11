3efa4a439b664057b7e82dcbebb20b34329598e7
Kyle Miller <kylemiller995@gmail.com>
2017-09-11 18:14:29 +0100

    add basic

---
+++ app/src/main/java/example/codeclan/com/employee_starter/Employee.java
@@ -0,0 +1,86 @@
+package example.codeclan.com.employee_starter;
+
+/**
+ * Created by user on 30/08/2017.
+ */
+
+import java.sql.ResultSet;
+
+import db.SqlRunner;
+
+public class Employee {
+    private int id;
+    private String name;
+    private double salary;
+    private Department department;
+
+    public Employee(String name, Department department, double salary) {
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void save() {
+        String sql = String.format("INSERT INTO employees (name, salary, department_id) VALUES ('%s', '%s', %d) ", name, salary, department);
+        this.id = SqlRunner.executeUpdate(sql);
+        SqlRunner.closeConnection();
+    }
+
+
+    public static void all() {
+        String sql = "Select * FROM employees;";
+        ResultSet rs = SqlRunner.executeQuery(sql);
+        try{
+            while ( rs.next() ){
+                String name = rs.getString("name");
+                System.out.println(name);
+                String salary = rs.getString("salary");
+                System.out.println(salary);
+                int department_id = rs.getInt("department_id");
+                System.out.println(department_id);
+            }
+        } catch(Exception e){
+            System.err.println(e.getClass().getName() + " : "+e.getMessage());
+            System.exit(0);
+        } finally{
+            SqlRunner.closeConnection();
+        }
+
+    }
+
+    public static void deleteall(){
+        String sql = "DELTE FROM employees;";
+        SqlRunner.executeUpdate(sql);
+        SqlRunner.closeConnection();
+    }
+
+    public void delete(){
+        String sql = String.format("DELETE FROM employees WHERE id = %d;", this.id);
+        SqlRunner.executeUpdate(sql);
+        SqlRunner.closeConnection();
+    }
+
+
+    public void update(){
+        String sql = String.format("UPDATE employees SET (name, salary, department_id) = ('%s', '%s', '%d') WHERE id = %d;", this.name, this.salary, this.department, this.id);
+        SqlRunner.executeUpdate(sql);
+        SqlRunner.closeConnection();
+    }
+
+}
